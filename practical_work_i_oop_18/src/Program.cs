6e8008d5543107493fb4569514cb6af6d1582a40
nicoguzman12 <9403538@alumnos.ufv.es>
2025-04-27 18:59:34 +0200

    filePath

--- practical_work_i_oop_18/src/Program.cs
+++ practical_work_i_oop_18/src/Program.cs
@@ -9,10 +9,7 @@ namespace AirUFV
             // Create an instance of the airport with 2 runways
             Airport airport = new Airport(2);
 
-            // Ask the user for the path to the CSV file
-            
-            Console.Write("Enter the path to the aircraft CSV file: (Aircraft_Status.csv) ");
-            string filePath = Console.ReadLine();
+            string filePath = "../../../Aircraft_Status.csv";
             // Load aircraft from the file into the airport
             airport.LoadAircraftFromFile(filePath);
 
